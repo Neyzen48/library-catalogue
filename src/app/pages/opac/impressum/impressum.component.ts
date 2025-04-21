105ca76abade40c1096baa818ba097b764e1c6ce
neyzen48 <oemer-faruk.torun@stud-provadis-hochschule.de>
2025-04-21 22:12:57 +0200

    add home page without content [update]

---
+++ src/app/pages/opac/impressum/impressum.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impressum',
+  standalone: false,
+  templateUrl: './impressum.component.html',
+  styleUrl: './impressum.component.css'
+})
+export class ImpressumComponent {
+
+}
