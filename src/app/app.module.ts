9c7b22e15ed35796a7c79fc4cbe99bc562027a59
neyzen48 <oemer-faruk.torun@stud-provadis-hochschule.de>
2025-04-21 22:11:48 +0200

    add home page without content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/opac/home/home.component';
+import { ImpressumComponent } from './pages/opac/impressum/impressum.component';
+import { PrivacyComponent } from './pages/opac/privacy/privacy.component';
+import { SettingsComponent } from './pages/opac/settings/settings.component';
+import { CatalogueComponent } from './pages/opac/catalogue/catalogue.component';
+import { ConfigurationComponent } from './pages/dashboard/configuration/configuration.component';
+import { OrdersComponent } from './pages/dashboard/orders/orders.component';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ImpressumComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrivacyComponent,
+    SettingsComponent,
+    CatalogueComponent,
+    ConfigurationComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIcon,
+    MatIcon,
+    MatButton
   ],
   providers: [],
   bootstrap: [AppComponent]
